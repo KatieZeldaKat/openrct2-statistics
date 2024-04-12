58a0bd8379afc1cdf4366a62c74176fcc07b7d87
Smitty Penman <smitty.penman@gmail.com>
2024-04-12 07:26:54 -0400

    simplify comment and move inline

--- src/helpers/apiToGameReleaseVersionMap.ts
+++ src/helpers/apiToGameReleaseVersionMap.ts
@@ -1,17 +1,10 @@
 /**
- * Current [april 2024] is v81, which requires at least 0.4.7
- * 78 is 0.4.6
- * 72 is 0.4.5
- * 70 is 0.4.4
- * 52 is 0.4.0
- */
-
-/**
  * Plugin api versions are bumped when there are meaningful changes to the plugin api.
  * This will return the minimum game release version that the api is compatible with.
  */
 export const getGameVersionFromApiVersion = (apiVersion: number): string | undefined => {
     // will need to update this as new versions are released
+    // Current [april 2024] is v81, which requires at least 0.4.7
     if (apiVersion >= 82) {
         return undefined;
     }
